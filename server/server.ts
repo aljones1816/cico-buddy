fc155ea81c6d0fc153a868e0edc225dc2344cc7d
Alan Jones <aljones1816@gmail.com>
2023-05-16 09:20:04 -0400

    modified some scripts

--- server/server.ts
+++ server/server.ts
@@ -13,6 +13,7 @@ app.use(express.json());
 
 const uri = process.env.ATLAS_URI ?? "";
 
+mongoose.set('debug', true);
 mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once("open", () => {
