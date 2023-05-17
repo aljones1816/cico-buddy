3c2534068aa5d68a8e0a0eef6149f5a730af1298
Alan <aljones1816@gmail.com>
2023-05-17 13:38:32 -0400

    place port number in dotenv file

--- server/server.ts
+++ server/server.ts
@@ -8,7 +8,7 @@ import { userlogRouter } from "./routes/userlog.ts";
 config();
 
 const app = express();
-const port = process.env.PORT || 5100;
+const port = process.env.PORT ?? 5000;
 
 app.use(cors());
 app.use(express.json());
