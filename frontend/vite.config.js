b269c20202cfd83e60eaad569ec9bedeb2f0f6a0
Alan <aljones1816@gmail.com>
2023-06-23 22:18:28 -0400

    adding for merge

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        proxy: {
-            "/api": "http://localhost:5100",
-        },
+  server: {
+    proxy: {
+      "/api": "http://localhost:8080",
     },
-    plugins: [react()],
+  },
+  plugins: [react()],
 });
